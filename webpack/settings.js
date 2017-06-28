12b515f0369eb9d100060c62abd230ffd65a6898
黄书伟 <hsw89520@163.com>
2017-06-28 17:33:54 +0800

    add webpack setting

---
+++ webpack/settings.js
@@ -0,0 +1,84 @@
+var path = require('path');
+
+exports.getSettings = function (env) {
+
+    var settings = {
+        // 通过代理访问的后台地址
+        proxyTarget:'http://localhost:2246/',
+
+        // 当前启动的端口
+        visitPort:'8055',
+
+        // 是否调试模式
+        isDebug:false,
+
+        // 是否启用js、css压缩
+        isMinimize:false,
+
+        outputFolderName: '', // 发布目录名称
+
+        path:{
+            // 输出目录路径
+            outputPath: '',
+
+            /*
+             * publish path
+             * （发布目录）
+             * */
+            publicPath: '',
+
+            /*
+             * node_modules path
+             */
+            node_modulesPath: path.resolve('./node_modules'),
+
+            /*
+             *公共文件
+             * */
+            libsPath: path.resolve('./libs'),
+        }
+    };
+
+
+    if (env && env.dev){
+
+        settings.isDebug = true;
+        settings.isMinimize = false;
+
+        settings.outputFolderName = 'dist';
+
+        settings.path.outputPath = "./";
+        settings.path.publicPath = "/webpack3-demo/"
+
+    }else if (env && env.build){
+
+        settings.isDebug = false;
+        settings.isMinimize = true;
+
+        settings.outputFolderName = 'dist';
+
+        settings.path.outputPath = "./";
+        settings.path.publicPath = "./"
+
+    }else if (env && env.dist){
+
+
+
+    }else{
+
+        if (!env){
+
+            console.error('--参数 env 不能为空--');
+
+        }else{
+
+            console.error('--没有指定的 env 标识--');
+        }
+
+        return false;
+    }
+
+
+    return settings;
+
+}
